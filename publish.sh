f1aabea1dc7c5150a127dd207dbad3eb94a345dc
lior baber <lior.baber@wework.com>
2019-05-01 16:25:05 +0300

    playing with versions

--- publish.sh
+++ publish.sh
@@ -4,15 +4,13 @@
 set -e
 
 export VERSION=$(/bin/bash ./increment_semver.sh -p $(cat ./version.py))
-
-npm install -g npm-cli-login
-rm -rf dist
-pip install --user --upgrade setuptools wheel twine
-python3 setup.py sdist bdist_wheel
-python3 -m twine upload -u $NPM_USERNAME -p $NPM_PASS --verbose --repository-url https://upload.pypi.org/legacy/ dist/*
-# expects  $NPM_USERNAME $NPM_PASS and $NPM_EMAIL env to be defined
-npm-cli-login
+echo $VERSION > ./version.py
+#npm install -g npm-cli-login
+#rm -rf dist
+#pip install --user --upgrade setuptools wheel twine
+#python3 setup.py sdist bdist_wheel
+#python3 -m twine upload -u $NPM_USERNAME -p $NPM_PASS --verbose --repository-url https://upload.pypi.org/legacy/ dist/*
+npm-cli-login -u $NPM_USERNAME -p $NPM_PASS -e $NPM_EMAIL
 npm version $VERSION
 npm publish --access public
-echo $VERSION > ./version.py
 git tag $VERSION
