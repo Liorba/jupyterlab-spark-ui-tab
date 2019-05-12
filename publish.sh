fdc2aebc527d878696b39ddec3091d5552c076b0
lior baber <lior.baber@wework.com>
2019-05-12 15:06:39 +0300

    fix circleci config

--- publish.sh
+++ publish.sh
@@ -4,9 +4,9 @@
 set -e
 
 
-#npm install -g npm-cli-login
-#rm -rf dist
-#pip install --user --upgrade setuptools wheel twine
+npm install -g npm-cli-login
+rm -rf dist
+pip install --user --upgrade setuptools wheel twine
 python3 setup.py sdist bdist_wheel
 python3 -m twine upload -u $NPM_USERNAME -p $NPM_PASS --verbose --repository-url https://upload.pypi.org/legacy/ dist/*
 npm-cli-login -u $NPM_USERNAME -p $NPM_PASS -e $NPM_EMAIL
